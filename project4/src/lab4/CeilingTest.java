9b1cf53a951a690f00055db39e9aa9027473c104
Ethan Pham <ethanpham652@gmail.com>
2026-02-26 01:40:06 -0600

    Update

---
+++ project4/src/lab4/CeilingTest.java
@@ -0,0 +1,14 @@
+package lab4;
+
+public class CeilingTest
+{
+  public static void main(String[] args)
+  {
+    System.out.println(Math.ceil(1.3));
+    System.out.println(Math.ceil(2.0));
+    System.out.println(Math.ceil(2.1));
+    System.out.println(Math.ceil(-2.1));
+  }
+}
+
+// Flowchart 2 is wrong because it will never reach weight > 3.5
